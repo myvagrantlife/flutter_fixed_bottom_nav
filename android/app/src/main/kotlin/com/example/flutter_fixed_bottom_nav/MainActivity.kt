8bedac554a0b1a94050ddb791d74107033167ab4
myvagrantlife <edward@myvagrantlife.com>
2020-08-21 23:52:42 +0200

    init new flutter project

---
+++ android/app/src/main/kotlin/com/example/flutter_fixed_bottom_nav/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fixed_bottom_nav
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
